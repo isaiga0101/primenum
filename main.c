d81d482f113a2877d5c7a20f4fb0d8a54ea2af13
isaiga0101 <gayfieldfamily@hotmail.com>
2015-09-26 20:35:28 -0700

    Added function that creates a custom length string by knowing the number of primes.

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 int num[39] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
 int current = 2;                                    // This variable defines what number you are deleting
 int delval;                                         // This variable defines which values to delete.
-int numOfPrs;                                       // Holds the number of Prime numbers
 
+void stringVar_create(int strlength);
 
 int main()
 {
+    int numOfPrs;                                   // Holds the number of Prime numbers
     printf("Looking for prime numbers under 40\n");
     for (int counter=1;counter>=39;counter ++;)     // Stop looping when we have reached the maximum number.
     {
@@ -18,8 +19,18 @@ int main()
             current += delval;                      // Change the variable to what number we are on.
 
         }
+        numOfPrs ++;                                // Add 1 to numOfPrs variable
     }
+
+    /* When the loop is done then print the numbers to a string created by
+    the stringVar_create() function.*/
+
 
 
     return 0;
+}
+
+void stringVar_create(int strlength) {
+    char * string[strlength];
+
 }
