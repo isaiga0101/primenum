4b9e1dfae6e9c17748cf6cec649ed6dbea422ca8
isaiga0101 <gayfieldfamily@hotmail.com>
2015-09-27 12:30:46 -0700

    Starting over using simpler approach. Starting to make a program that will check to see if a number is prime.

--- main.c
+++ main.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-int num[39] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
-int current = 2;                                    // This variable defines what number you are deleting
-int delval;                                         // This variable defines which values to delete.
+#include <math.h>
 
-void stringVar_create(int strlength);
+int current = 2;                                    // This variable defines what number you are deleting
+int inNumHolder = 0;
+
+void primeCheck(int prime);
 
 int main()
 {
     int numOfPrs;                                   // Holds the number of Prime numbers
-    printf("Looking for prime numbers under 40\n");
-    for (int counter=1;counter>=39;counter ++;)     // Stop looping when we have reached the maximum number.
-    {
-        delval = num[counter];                      // Make delval equal to the number that shows what value we want to delete.
-        current = num[counter];                     // Change the current variable to show what number we are currently on
-        while(current <= 40) {                      // Loop only while we are still on a number we can delete
-            num[delval + current] = 0;              // Delete the value that we know are not prime.
-            current += delval;                      // Change the variable to what number we are on.
-
-        }
-        numOfPrs ++;                                // Add 1 to numOfPrs variable
-    }
-
-    /* When the loop is done then print the numbers to a string created by
-    the stringVar_create() function.*/
+    printf("Input an integer above 1 to check if it is prime.\n");
+    scanf("%d",&inNumHolder);
+    printf("The number you pressed is %d.\nChecking that number.",inNumHolder);
 
 
 
     return 0;
 }
 
-void stringVar_create(int strlength) {
-    char * string[strlength];
-
-}
