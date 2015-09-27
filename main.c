d17981ef3be8d9bc803934b3a75f13931670774e
isaiga0101 <gayfieldfamily@hotmail.com>
2015-09-27 14:34:30 -0700

    Added the prime checker function that will check if the number is prime.

--- main.c
+++ main.c
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include <math.h>
 
-int current = 2;                                    // This variable defines what number you are deleting
-int inNumHolder = 0;
+int inNumHolder = 0;                                            // Variable that will hold the int that is inputted
 
-void primeCheck(int prime);
+int primeChecker(int prime);                                    // 0 = False, 1 = True
 
 int main()
 {
-    int numOfPrs;                                   // Holds the number of Prime numbers
-    printf("Input an integer above 1 to check if it is prime.\n");
-    scanf("%d",&inNumHolder);
-    printf("The number you pressed is %d.\nChecking that number.",inNumHolder);
+    while(1)
+    {
+        printf("Input an integer above 1 to check if it is prime.\n");
+        scanf("%d",&inNumHolder);
+        printf("The number you pressed is %d.\nChecking that number.\n",inNumHolder);
 
-
+        if (primeChecker(inNumHolder) == 0) printf("%d is not a prime number.\n",inNumHolder);
+        else printf("%d is a prime number.\n", inNumHolder);
+    }
 
     return 0;
 }
 
+int primeChecker(int prime) {
+    for(int i=2; i < sqrt(prime);i ++;)
+    {
+        if (prime % i == 0) return 0;
+        else return 1;
+    }
+}
