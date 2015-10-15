a0e37f1267bd2c7a904519126311754c0ded1aef
isaiga0101 <gayfieldfamily@hotmail.com>
2015-10-14 17:44:37 -0700

    Changes the %d in the second printf() function in the while loop to %f so that it will display the number right.

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main()
 
         printf("Input an integer above 1 to check if it is prime.\n");                  // Prompts the user to input an integer
         scanf("%d",&inNumHolder);                                                       // Stores the value of the integer to inNumHolder variable
-        printf("The number you pressed is %d.\nChecking that number.\n",inNumHolder);   // Outputs what number is pressed.
+        printf("The number you pressed is %f.\nChecking that number.\n",inNumHolder);   // Outputs what number is pressed.
 
         // if/else statement uses primeChecker() function to check if the number is prime and outputs whether it is prime.
         if (primeChecker(inNumHolder) == 0) printf("%d is not a prime number.\n",inNumHolder);
